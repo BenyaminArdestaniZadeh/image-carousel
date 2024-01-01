c7daf2b4474460d8d913c0cf2d8c619f663bf951
benyamin ardestani zadeh <benyaminardestanizadeh@outlook.com>
2024-01-01 20:58:16 +0330

    chore: define data for images

---
+++ src/components/image-carousel/data.ts
@@ -0,0 +1,15 @@
+export const ImaageData: ImageProps[] = [
+  { src: "./image/image-1.jpg", alt: "background-1" },
+  { src: "./image/image-2.jpg", alt: "background-2" },
+  { src: "./image/image-3.jpg", alt: "background-3" },
+  { src: "./image/image-4.jpg", alt: "background-4" },
+  { src: "./image/image-5.jpg", alt: "background-5" },
+  { src: "./image/image-6.jpg", alt: "background-6" },
+];
+
+export type ImageProps = {
+  src: string;
+  alt: string;
+};
+
+// console.log(ImaageData);
